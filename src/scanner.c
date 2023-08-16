25cba9b18b42742a78486c31793b172cec36061e
Mahtab (Mattie) Nejati <mahtab.nejati@gmail.com>
2023-08-16 11:09:53 -0400

    formatting

--- src/scanner.c
+++ src/scanner.c
@@ -1,46 +1,61 @@
 #include <tree_sitter/parser.h>
 #include <wctype.h>
 
-enum TokenType { BRACKET_ARGUMENT, BRACKET_COMMENT, LINE_COMMENT };
+enum TokenType
+{
+  BRACKET_ARGUMENT,
+  BRACKET_COMMENT,
+  LINE_COMMENT
+};
 
 static void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
 
 static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 
-static void skip_wspace(TSLexer *lexer) {
-  while (iswspace(lexer->lookahead)) {
+static void skip_wspace(TSLexer *lexer)
+{
+  while (iswspace(lexer->lookahead))
+  {
     skip(lexer);
   }
 }
 
-static bool is_bracket_argument(TSLexer *lexer) {
-  if (lexer->lookahead != '[') {
+static bool is_bracket_argument(TSLexer *lexer)
+{
+  if (lexer->lookahead != '[')
+  {
     return false;
   }
   advance(lexer);
 
   int open_level = 0;
-  while (lexer->lookahead == '=') {
+  while (lexer->lookahead == '=')
+  {
     ++open_level;
     advance(lexer);
   }
 
-  if (lexer->lookahead != '[') {
+  if (lexer->lookahead != '[')
+  {
     return false;
   }
 
-  while (lexer->lookahead != '\0') {
+  while (lexer->lookahead != '\0')
+  {
     advance(lexer);
-    if (lexer->lookahead == ']') {
+    if (lexer->lookahead == ']')
+    {
       advance(lexer);
 
       int close_level = 0;
-      while (lexer->lookahead == '=') {
+      while (lexer->lookahead == '=')
+      {
         ++close_level;
         advance(lexer);
       }
 
-      if (lexer->lookahead == ']' && close_level == open_level) {
+      if (lexer->lookahead == ']' && close_level == open_level)
+      {
         advance(lexer);
         return true;
       }
@@ -49,23 +64,31 @@ static bool is_bracket_argument(TSLexer *lexer) {
   return false;
 }
 
-static bool scan(void *payload, TSLexer *lexer, bool const *valid_symbols) {
+static bool scan(void *payload, TSLexer *lexer, bool const *valid_symbols)
+{
   skip_wspace(lexer);
 
-  if (lexer->lookahead != '#' && valid_symbols[BRACKET_ARGUMENT]) {
-    if (is_bracket_argument(lexer)) {
+  if (lexer->lookahead != '#' && valid_symbols[BRACKET_ARGUMENT])
+  {
+    if (is_bracket_argument(lexer))
+    {
       lexer->result_symbol = BRACKET_ARGUMENT;
       return true;
     }
   }
   if (lexer->lookahead == '#' &&
-      (valid_symbols[BRACKET_COMMENT] || valid_symbols[LINE_COMMENT])) {
+      (valid_symbols[BRACKET_COMMENT] || valid_symbols[LINE_COMMENT]))
+  {
     advance(lexer);
-    if (is_bracket_argument(lexer)) {
+    if (is_bracket_argument(lexer))
+    {
       lexer->result_symbol = BRACKET_COMMENT;
       return true;
-    } else {
-      while (lexer->lookahead != '\n' && lexer->lookahead != '\0') {
+    }
+    else
+    {
+      while (lexer->lookahead != '\n' && lexer->lookahead != '\0')
+      {
         advance(lexer);
       }
       lexer->result_symbol = LINE_COMMENT;
@@ -81,7 +104,8 @@ void *tree_sitter_cmake_external_scanner_create() { return NULL; }
 void tree_sitter_cmake_external_scanner_destroy(void *payload) {}
 
 unsigned tree_sitter_cmake_external_scanner_serialize(void *payload,
-                                                      char *buffer) {
+                                                      char *buffer)
+{
   return 0;
 }
 
@@ -90,6 +114,7 @@ void tree_sitter_cmake_external_scanner_deserialize(void *payload,
                                                     unsigned length) {}
 
 bool tree_sitter_cmake_external_scanner_scan(void *payload, TSLexer *lexer,
-                                             bool const *valid_symbols) {
+                                             bool const *valid_symbols)
+{
   return scan(payload, lexer, valid_symbols);
 }
